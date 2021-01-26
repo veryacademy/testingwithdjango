659a10d9909b9fea13c4e3c2788dcb429d298585
veryacademy <veryacademymail@gmail.com>
2021-01-26 12:37:26 +0000

    nc

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# hkajdhaskjdh
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255)
+
+    def __str__(self):
+        return self.title
